4e3ccf4c5572f512468e621fdedfd1ccfe5dbc3a
Kira <kiracabezas@gmail.com>
2022-04-03 10:55:49 +0200

    Initial commit

---
+++ src/main/java/es/iessoterohernandez/BBaker/BBakerApplication.java
@@ -0,0 +1,13 @@
+package es.iessoterohernandez.BBaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BBakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/iessoterohernandez/BBaker/BBakerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iessoterohernandez.BBaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BBakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
